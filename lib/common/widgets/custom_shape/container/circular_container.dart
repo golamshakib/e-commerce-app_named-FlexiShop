2f5364da19287f0c51b7de9ad414b0de23f3b0aa
Your Name <you@example.com>
2024-11-06 12:23:21 +0600

    complete the home-screen clip path design

---
+++ lib/common/widgets/custom_shape/container/circular_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/colors.dart';
+
+class YCircularContainer extends StatelessWidget {
+  const YCircularContainer({
+    super.key,
+    this.child,
+    this.width = 400.0,
+    this.height = 400.0,
+    this.padding = 0.0,
+    this.radius = 400.0,
+    this.backgroundColor = YColors.white,
+  });
+
+  final double? width;
+  final double? height;
+  final double padding;
+  final double radius;
+  final Color backgroundColor;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: backgroundColor,
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../../../../common/widgets/custom_shape/container/primary_header_container.dart';
+
+
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/home';
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            YPrimaryHeaderContainer(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
